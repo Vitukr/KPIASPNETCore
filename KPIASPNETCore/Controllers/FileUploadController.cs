96fe36c66d14a5a45cb9e095336ee76bc2a9fdc0
Vitalii Yarema <vyarema@windowslive.com>
2019-03-09 10:14:58 +0200

    Upload files

---
+++ KPIASPNETCore/Controllers/FileUploadController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KPIASPNETCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase // IHostingEnvironment env
+    {
+        IHostingEnvironment _env;
+
+        public FileUploadController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost("Uploadfile")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var folderPath = Path.Combine(_env.WebRootPath, "images"); //Path.GetTempFileName();
+            if(!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(Path.Combine(folderPath, formFile.FileName), FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size, folderPath });
+        }
+    }
+}
--- KPIASPNETCore/Controllers/HomeController.cs
+++ KPIASPNETCore/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using KPIASPNETCore.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace KPIASPNETCore.Controllers
 {
@@ -12,6 +14,40 @@ namespace KPIASPNETCore.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public async Task<IActionResult> Uploadfile(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size, filePath });
+        }
+
+        public IActionResult ImageBook()
+        {
+            return View();
+        }
+
+        public IActionResult Intro()
+        {            
             return View();
         }
 
